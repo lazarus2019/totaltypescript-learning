6d95fb3ee5dfac5a33cce7f11190fe66854c04ac
Thai Son <thaisonbk2019@gmail.com>
2023-11-12 20:02:25 +0700

    init: beginner typescript tutorial base source

---
+++ beginners-typescript-tutorial/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+export default defineConfig({
+  test: {
+    include: [
+      'src/**/*{problem,solution,explainer}*.ts',
+      'src/**/**/*{problem,solution,explainer}*.ts',
+    ],
+    setupFiles: [path.resolve(__dirname, 'scripts/setup.ts')],
+    passWithNoTests: true,
+    environment: 'jsdom',
+  },
+  plugins: [tsconfigPaths()],
+});
